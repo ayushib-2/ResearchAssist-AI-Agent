c44b01f712e7129ec39c6da3ae6c99d9b5256d2d
ayushib-2 <109623235+ayushib-2@users.noreply.github.com>
2025-07-03 19:08:36 -0700

    removed library

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from dotenv import load_dotenv
 from pydantic import BaseModel
 from langchain_openai import ChatOpenAI
-from langchain_anthropic import ChatAnthropic
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import PydanticOutputParser
 from langchain.agents import create_tool_calling_agent
